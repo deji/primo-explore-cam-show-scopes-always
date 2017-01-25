05a5fab3e2e3790d0b97c8f31a83f239ff7c6a73
Oladeji Famakinwa <ojf27@cam.ac.uk>
2017-01-25 10:44:07 +0000

    first commit

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+/** Scopes **/
+app.component('prmSearchBarAfter', {
+    bindings: {parentCtrl: '<'},
+    controller: 'SearchBarAfterController'
+});
+ 
+app.controller('SearchBarAfterController', ['angularLoad', function (angularLoad) {
+    var vm = this;
+    vm.parentCtrl.showTabsAndScopes = true;
+}]);
+
